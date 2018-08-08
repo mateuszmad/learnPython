79255bcf547d119eba0b43653fb5fe1a6423678b
Mateusz Madej <34857716+mateuszmad@users.noreply.github.com>
2018-08-08 21:08:16 +0200

    Add files via upload

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,10 @@
+
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
